ff985e4c56800ac285210a0732edb87df3128f60
MarkKu7 <marksrc7@gmail.com>
2021-08-27 15:19:18 -0700

    working project meeting all requirements

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,40 @@
-//Business Logic for pizza selection
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.price = 0;
 }
 
-Pizza.prototype.ToppingPrice = function() {
+Pizza.prototype.toppingCost = function() {
   for (let i = 0; i < this.toppings.length; i++) {
     this.price += 1;
   }
 }
 
-Pizza.prototype.SizePrice= function() {
+Pizza.prototype.sizeCost = function() {
   const stPrice = 10;
-  if (this.size === "Large") {
-    this.price = stPrice + 5;
-  } else if (this.size === "Medium") {
-    this.price = stPrice + 3;
-  } else if (this.sizse === "Small") {
-    this.price = stPrice + 1; 
+    if (this.size === "Large (14 inches)") {
+      this.price = basePrice + 5;
+    } else if (this.size === "Medium (12 inches)") {
+      this.price = stPrice + 3;
+    } else if (this.size === "Small (10 inches)") {
+      this.price = stPrice + 1;
+    }
   }
-}
 
 Pizza.prototype.totalPrice = function() {
-  this.ToppingPrice;
-  this.SizePrice;
+  this.sizeCost();  
+  this.toppingCost(); 
 }
-
-//UI 
-
-$(document).ready(function() {
-  $("form#selectSize").submit(function(event) {
+$(document).ready(function(){
+  $("form#SelectOptions").submit(function(event){
     event.preventDefault();
-    const size = $("#size").val
-    const toppings = $('input:checkbox[name=toppings]:checked').map(function() {
-      return this.value;
-    });
+      const size = $("#size").val();
+      const toppings = $('input:checkbox[name=toppings]:checked').map(function() {
+        return this.value;
+      });
 
-    let finalPrice = new Pizza(size, toppings);
-    finalPrice.totalPrice();
-    $("#finalprice").html(`<p>Your pizza costs $${finalPrice.price}</p>`);
+      let displayPrice = new Pizza(size, toppings);
+      displayPrice.totalPrice();
+      $("#totalprice").html(`<p>Your pizza costs $${displayPrice.price}</p>`);
   });
 });
-  
