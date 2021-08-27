26d60171fa606b6daa842c63806db5e96c91d347
MarkKu7 <marksrc7@gmail.com>
2021-08-27 16:12:52 -0700

    added css bg and font change

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,43 @@
+//Business Logic section
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
   this.price = 0;
 }
 
-Pizza.prototype.toppingCost = function() {
+Pizza.prototype.toppingCost = function () {
   for (let i = 0; i < this.toppings.length; i++) {
     this.price += 1;
   }
 }
 
-Pizza.prototype.sizeCost = function() {
+Pizza.prototype.sizeCost = function () {
   const stPrice = 10;
-    if (this.size === "Large (14 inches)") {
-      this.price = stPrice + 5;
-    } else if (this.size === "Medium (12 inches)") {
-      this.price = stPrice + 3;
-    } else {
-      this.price = stPrice + 1;
-    }
+  if (this.size === "Large (14 inches)") {
+    this.price = stPrice + 5;
+  } else if (this.size === "Medium (12 inches)") {
+    this.price = stPrice + 3;
+  } else {
+    this.price = stPrice + 1;
   }
-
-Pizza.prototype.totalPrice = function() {
-  this.sizeCost();  
-  this.toppingCost(); 
 }
-$(document).ready(function(){
-  $("form#SelectOptions").submit(function(event){
-    event.preventDefault();
-      const size = $("#size").val();
-      const toppings = $('input:checkbox[name=toppings]:checked').map(function() {
-        return this.value;
-      });
 
-      let displayPrice = new Pizza(size, toppings);
-      displayPrice.totalPrice();
-      $("#totalprice").html(`<p>Your total is $${displayPrice.price}</p>`);
+Pizza.prototype.totalPrice = function () {
+  this.sizeCost();
+  this.toppingCost();
+}
+
+//UI section
+$(document).ready(function () {
+  $("form#SelectOptions").submit(function (event) {
+    event.preventDefault();
+    const size = $("#size").val();
+    const toppings = $('input:checkbox[name=toppings]:checked').map(function () {
+      return this.value;
+    });
+
+    let displayPrice = new Pizza(size, toppings);
+    displayPrice.totalPrice();
+    $("#totalprice").html(`<p>Your total is $${displayPrice.price}</p>`);
   });
 });
