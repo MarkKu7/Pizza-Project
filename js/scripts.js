eaf3029f9d7a0197b137357aa3ce9532527523a8
MarkKu7 <marksrc7@gmail.com>
2021-08-27 11:09:01 -0700

    added code in js for business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,29 @@
 //Business Logic for pizza selection
+let price = 0;
+
 function Pizza(size, toppings) {
   this.size = size;
   this.toppings = toppings;
+}
+
+Pizza.prototype.ToppingPrice = function() {
+  for (let i = 0; i < this.toppings.length; i++) {
+    this.price += 1;
+  }
+}
+
+Pizza.prototype.SizePrice= function() {
+  const stPrice = 10;
+  if (this.size === "Large") {
+    this.price = stPrice + 5;
+  } else if (this.size === "Medium") {
+    this.price = stPrice + 3;
+  } else if (this.sizse === "Small") {
+    this.price = stPrice + 1; 
+  }
+}
+
+Pizza.prototype.totalPrice = function() {
+  this.SizePrice;
+  this.ToppingPrice;
 }
